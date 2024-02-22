d84644dcbda189e5c9dba42e9ce6f2bce461b105
Mervesenaa <154423223+Mervesenaa@users.noreply.github.com>
2024-02-22 10:54:46 +0300

    İki VC'ye sahip basit bir Simpson ailesi appi
    Navigation Controllerı öğrenme sürecimde yaptığım basit bir uygulama.

---
+++ Simpsons/SimpsonBook/DetailsVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailsVC.swift
+//  SimpsonBook
+//
+//  Created by Merve Sena on 2.02.2024.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!    
+    @IBOutlet weak var jobLabel: UILabel!
+    
+    var selectedSimpson : Simpson?
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = selectedSimpson?.name
+        jobLabel.text = selectedSimpson?.job
+        imageView.image = selectedSimpson?.image
+        
+
+    }
+    
+}
---
+++ Simpsons/SimpsonBook/Simpson.swift
@@ -0,0 +1,24 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Merve Sena on 2.02.2024.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName:String,simpsonJob:String,simpsonImage:UIImage ){
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+    }
+    
+}
